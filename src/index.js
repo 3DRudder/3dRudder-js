0ee2c5b7b1c212ffeb846aeef393caa64179f397
mvivant <mvivant@3drudder.com>
2019-12-05 16:39:56 +0100

    Fix close and wait 3 sec on discovery

--- src/index.js
+++ src/index.js
@@ -279,7 +279,7 @@ Sdk.prototype.onMessage = function (message) {
 */
 Sdk.prototype.stopConnection = function () {
     if (this.connection) {
-        this.connection.close();
+        this.connection.close(1000);
     }
 }
 
@@ -390,7 +390,7 @@ Sdk.prototype.startDiscovery = function () {
     .then((desc) => {
         console.log("setlocaldesc");
         localConnection.setLocalDescription(desc);        
-        setTimeout(() => _this.stopDiscovery(localConnection), 1000);
+        setTimeout(() => _this.stopDiscovery(localConnection), 3000);
     }, (error) => {
         console.log("error create offer" + error);
     });
